49032954ea7c515395e34338f5fdf18da4ca4424
Maris Svirksts <maris.svirksts@gmail.com>
2023-08-15 19:36:44 +0300

    Additional logging

--- src/main/scala/LuckyNumbersGame.scala
+++ src/main/scala/LuckyNumbersGame.scala
@@ -1,10 +1,12 @@
 import scala.util.Random
 import scala.concurrent.{Future, ExecutionContext}
 import ExecutionContext.Implicits.global
+import org.slf4j.LoggerFactory
 
 object LuckyNumbersGame {
 
   val MaxRandomNumber: Int = 1000000
+  private val logger = LoggerFactory.getLogger(getClass)
 
   case class Player(id: Int, luckyNumber: Int)
 
@@ -45,13 +47,29 @@ object LuckyNumbersGame {
    */
   def play(numberOfPlayers: Int): Future[Seq[Results]] = {
     require(numberOfPlayers > 0, "Number of players must be greater than 0")
+    
+    logger.info(s"Starting play function for $numberOfPlayers players")
+    
     for {
       results <- addPlayersWithResults(numberOfPlayers)
+      _ = logger.info(s"Results for players obtained: $results")
+      
       botResult <- addPlayersWithResults(1).map(_.head.result)
+      _ = logger.info(s"Bot result obtained: $botResult")
     } yield {
+      logger.info("Filtering winners based on bot result")
       val winners = results.filter(_.result > botResult)
+      
+      logger.info(s"Winners filtered: $winners")
+      
       val sortedWinners = winners.sortBy(-_.result)
-      sortedWinners.zipWithIndex.map { case (result, index) => result.copy(position = index + 1) }
+      logger.info(s"Winners sorted: $sortedWinners")
+      
+      val finalResults = sortedWinners.zipWithIndex.map { case (result, index) => result.copy(position = index + 1) }
+      
+      logger.info(s"Final results: $finalResults")
+      
+      finalResults
     }
   }
 }
