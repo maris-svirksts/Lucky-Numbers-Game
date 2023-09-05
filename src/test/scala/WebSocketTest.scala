a94fd95a13049f0abcad6a82aa7f9ac581248aa0
Maris Svirksts <maris.svirksts@gmail.com>
2023-09-06 00:56:37 +0300

    Refactoring: wsClient to a new function

--- src/test/scala/WebSocketTest.scala
+++ src/test/scala/WebSocketTest.scala
@@ -12,36 +12,39 @@ import org.slf4j.LoggerFactory
 class WebSocketServerSpec extends AnyWordSpec with Matchers with ScalatestRouteTest {
 
   // Define the WebSocket route
-  val route: Route = path("game") {
+  private val route: Route = path("game") {
     handleWebSocketMessages(WebSocketServer.websocketFlow)
   }
 
-  val logger = LoggerFactory.getLogger(getClass)
+  private val logger = LoggerFactory.getLogger(getClass)
+
+  private def initiateWebSocketClient: WSProbe = {
+    val wsClient = WSProbe()
+    WS("/game", wsClient.flow) ~> route
+    wsClient
+  }
 
   "The Web Server / PingPong" must {
     "respond with a pong message when receiving a ping message" in {
       // Create WebSocket client probe
-      val wsClient = WSProbe()
+      val wsClient = initiateWebSocketClient
 
       // Test setup for WebSocket communication
-      WS("/game", wsClient.flow) ~> route ~>
-      check {
-          val pingMessage = PingMessage(id = 1, System.currentTimeMillis())
-          val messageToSend = TextMessage(pingMessage.toJson.compactPrint)
+      val pingMessage = PingMessage(id = 1, System.currentTimeMillis())
+      val messageToSend = TextMessage(pingMessage.toJson.compactPrint)
 
-          // Log the sent message
-          logger.info(s"Sending message: $messageToSend")
+      // Log the sent message
+      logger.info(s"Sending message: $messageToSend")
 
-          wsClient.sendMessage(messageToSend)
-          wsClient.expectMessage() match {
-          case TextMessage.Strict(text) =>
-              logger.info(s"Received message: $text")
-              val receivedPong = text.parseJson.convertTo[PongMessage]
-              receivedPong.requestId shouldBe pingMessage.id
+      wsClient.sendMessage(messageToSend)
+      wsClient.expectMessage() match {
+        case TextMessage.Strict(text) =>
+          logger.info(s"Received message: $text")
+          val receivedPong = text.parseJson.convertTo[PongMessage]
+          receivedPong.requestId shouldBe pingMessage.id
 
-          case other =>
-              fail(s"Unexpected message type received: $other")
-          }
+        case other =>
+          fail(s"Unexpected message type received: $other")
       }
     }
   }
@@ -51,87 +54,80 @@ class WebSocketServerSpec extends AnyWordSpec with Matchers with ScalatestRouteT
     "return correct results on Play request" in {
       logger.info("Testing Play request...")
 
-      // Create WebSocket client probe
-      val wsClient = WSProbe()
-
       // Test setup for WebSocket communication
-      WS("/game", wsClient.flow) ~> route ~>
-      check {
-        // Send Play request
-        val playMessage = PlayMessage(3)
-        val playMessageJson = JsObject(
-          "message_type" -> JsString("request.play"),
-          "players" -> JsNumber(playMessage.players)
-        )
+      val wsClient = initiateWebSocketClient
+   
+      // Send Play request
+      val playMessage = PlayMessage(3)
+      val playMessageJson = JsObject(
+        "message_type" -> JsString("request.play"),
+        "players" -> JsNumber(playMessage.players)
+      )
 
-        wsClient.sendMessage(playMessageJson.compactPrint)
-        logger.debug(s"Sent message: ${playMessageJson.compactPrint}")
+      wsClient.sendMessage(playMessageJson.compactPrint)
+      logger.debug(s"Sent message: ${playMessageJson.compactPrint}")
 
-        // Check response from the server
-        wsClient.expectMessage() match {
-          case TextMessage.Strict(text) =>
-            val jsonResponse = text.parseJson.asJsObject
-            jsonResponse.fields.get("message_type") match {
-              case Some(JsString("response.results")) =>
-                // Directly extract the results array from jsonResponse
-                jsonResponse.fields.get("results") match {
-                  case Some(JsArray(resultsArray)) =>
-                    val results = resultsArray.toList.map(_.convertTo[Result])
-                    results.size should (be >= 0 and be <= 3)  // there can be between 0 to 3 results
+      // Check response from the server
+      wsClient.expectMessage() match {
+        case TextMessage.Strict(text) =>
+          val jsonResponse = text.parseJson.asJsObject
+          jsonResponse.fields.get("message_type") match {
+            case Some(JsString("response.results")) =>
+              // Directly extract the results array from jsonResponse
+              jsonResponse.fields.get("results") match {
+                case Some(JsArray(resultsArray)) =>
+                  val results = resultsArray.toList.map(_.convertTo[Result])
+                  results.size should (be >= 0 and be <= 3)  // there can be between 0 to 3 results
 
-                    // Verify positions are distinct and in ascending order
-                    results.map(_.position) shouldBe (1 to results.size).toList
+                  // Verify positions are distinct and in ascending order
+                  results.map(_.position) shouldBe (1 to results.size).toList
 
-                    logger.debug(s"Received response: $text")
+                  logger.debug(s"Received response: $text")
                     
-                  case None =>
-                    fail("No results key found in the response.")
-                  case _ =>
-                    fail("Results key in the response was not an array.")
-                }
+                case None =>
+                  fail("No results key found in the response.")
+                case _ =>
+                  fail("Results key in the response was not an array.")
+              }
 
-              case Some(other) => 
-                fail(s"Unexpected message_type value: $other")
+            case Some(other) => 
+              fail(s"Unexpected message_type value: $other")
 
-              case None => 
-                fail(s"message_type key not found in the response: $jsonResponse")
-            }
+            case None => 
+              fail(s"message_type key not found in the response: $jsonResponse")
+          }
 
-          case _ =>
-            fail("Received unexpected message type")
-        }
+        case _ =>
+          fail("Received unexpected message type")
       }
     }
 
     // Additional test for error handling
     "return an error for zero players" in {
-      val wsClient = WSProbe()
+      val wsClient = initiateWebSocketClient
 
-      WS("/game", wsClient.flow) ~> route ~>
-      check {
-        val playMessage = PlayMessage(0)
-        val playMessageJson = JsObject(
-          "message_type" -> JsString("request.play"),
-          "players" -> JsNumber(playMessage.players)
-        )
+      val playMessage = PlayMessage(0)
+      val playMessageJson = JsObject(
+        "message_type" -> JsString("request.play"),
+        "players" -> JsNumber(playMessage.players)
+      )
 
-        wsClient.sendMessage(playMessageJson.compactPrint)
-        val response = wsClient.expectMessage()
-        response.isText shouldBe true
+      wsClient.sendMessage(playMessageJson.compactPrint)
+      val response = wsClient.expectMessage()
+      response.isText shouldBe true
 
-        val jsonResponse = response.asTextMessage.getStrictText.parseJson.asJsObject
-        jsonResponse.fields.get("message_type") match {
-          case Some(JsString("response.error")) =>
-            // Expect a specific error message for zero players
-            val errorMessage = jsonResponse.fields("message").convertTo[String]
-            errorMessage should include("Number of players must be greater than 0")
+      val jsonResponse = response.asTextMessage.getStrictText.parseJson.asJsObject
+      jsonResponse.fields.get("message_type") match {
+        case Some(JsString("response.error")) =>
+          // Expect a specific error message for zero players
+          val errorMessage = jsonResponse.fields("message").convertTo[String]
+          errorMessage should include("Number of players must be greater than 0")
 
-          case Some(other) => 
-            fail(s"Unexpected message_type value: $other")
+        case Some(other) => 
+          fail(s"Unexpected message_type value: $other")
 
-          case None => 
-            fail(s"message_type key not found in the response: $jsonResponse")
-        }
+        case None => 
+          fail(s"message_type key not found in the response: $jsonResponse")
       }
     }
   }
